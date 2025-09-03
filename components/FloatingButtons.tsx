4fb7594f361a42a2d4f759700088411e145aea82
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-04 00:29:05 +0530

    Update FloatingButtons.tsx

--- components/FloatingButtons.tsx
+++ components/FloatingButtons.tsx
@@ -1,25 +1,17 @@
-"use client";
+import React from "react";
+import { FaShoppingCart, FaHome } from "react-icons/fa";
 
-import Link from "next/link";
-import { Home, ShoppingCart } from "lucide-react";
-
-export default function FloatingButtons() {
+const FloatingButtons = () => {
   return (
-    <div className="fixed bottom-4 right-4 flex flex-col gap-3">
-      <Link
-        href="/cart"
-        className="flex h-12 w-12 items-center justify-center rounded-full border bg-white shadow-md"
-        aria-label="Cart"
-      >
-        <ShoppingCart />
-      </Link>
-      <Link
-        href="/"
-        className="flex h-12 w-12 items-center justify-center rounded-full border bg-white shadow-md"
-        aria-label="Home"
-      >
-        <Home />
-      </Link>
-    </div>
+    <>
+      <button className="fab">
+        <FaHome />
+      </button>
+      <button className="fab mt-16">
+        <FaShoppingCart />
+      </button>
+    </>
   );
-}
+};
+
+export default FloatingButtons;
