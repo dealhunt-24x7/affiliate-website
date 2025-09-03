1ef12f83c1973a83aa83ac0e4e7c160c387214e5
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-03 13:56:12 +0530

    Update SpecsTable.tsx

--- components/SpecsTable.tsx
+++ components/SpecsTable.tsx
@@ -3,18 +3,18 @@ export function SpecsTable({ specs }: { specs: Record<string, string> }) {
     <table className="min-w-full border-collapse border border-gray-300">
       <thead>
         <tr>
-          <th className="border p-2 bg-gray-100 text-left">Specification</th>
-          <th className="border p-2 bg-gray-100 text-left">Details</th>
+          <th className="border p-2">Specification</th>
+          <th className="border p-2">Details</th>
         </tr>
       </thead>
       <tbody>
         {Object.entries(specs).map(([key, value]) => (
           <tr key={key}>
-            <td className="border p-2 font-medium">{key}</td>
+            <td className="border p-2">{key}</td>
             <td className="border p-2">{value}</td>
           </tr>
         ))}
       </tbody>
     </table>
-  );
+  )
 }
