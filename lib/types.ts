f1ff99db67f2f658cb46f01ca321fe242e1c419e
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-03 10:57:56 +0530

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,1 +1,20 @@
+export interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  rating: number;
+  image: string;
+  affiliateLink: string;
+}
 
+export interface BlogPost {
+  id: number;
+  title: string;
+  slug: string;
+  excerpt: string;
+  content: string;
+  cover: string;
+  tags: string[];
+  date: string;
+}
