d50d0ea71a323be470d2b3094edd49e14ad9b891
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-03 10:56:11 +0530

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -1,1 +1,10 @@
+import productsData from "../data/products.json";
+import { Product } from "./types";
 
+export const getProducts = (): Product[] => {
+  return productsData;
+};
+
+export const getProductById = (id: number): Product | undefined => {
+  return productsData.find((product) => product.id === id);
+};
