15e8c88702739456d5bf8b68b7e2577d1257b583
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-03 10:54:59 +0530

    Update blog.ts

--- lib/blog.ts
+++ lib/blog.ts
@@ -1,1 +1,10 @@
+import blogPostsData from "../data/blog.json";
+import { BlogPost } from "./types";
 
+export const getBlogPosts = (): BlogPost[] => {
+  return blogPostsData;
+};
+
+export const getBlogPostBySlug = (slug: string): BlogPost | undefined => {
+  return blogPostsData.find((post) => post.slug === slug);
+};
