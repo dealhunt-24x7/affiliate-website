17788a1be85f9b806c583102e648ec9656562640
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-03 11:48:12 +0530

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-6 mt-8">
-      <div className="container mx-auto text-center">
-        <p>© {new Date().getFullYear()} DealHunt. All rights reserved.</p>
-      </div>
-    </footer>
-  );
-}
