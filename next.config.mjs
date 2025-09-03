79e3d51492891b8392b90dee98f20214fa1df9bb
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-03 11:13:32 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-
   images: {
-    domains: [
-      "images.unsplash.com",
-      "via.placeholder.com",
-      "example.com"
-    ],
-  },
-
-  experimental: {
-    appDir: true, // Next.js 13+ ke liye
+    domains: ["via.placeholder.com"], // update domains if needed
   },
 };
 
