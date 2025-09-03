49f2db476306cd675cdc0ddcac90646e55d15690
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-03 10:54:07 +0530

    Update analytics.ts

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,1 +1,4 @@
-
+export const trackAffiliateClick = (link: string) => {
+  console.log(`Affiliate link clicked: ${link}`);
+  // TODO: integrate with analytics provider (Google Analytics, Plausible, etc.)
+};
