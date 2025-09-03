b336a0d59526347d0e89fe49bee70de82fe252cf
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-03 20:40:34 +0530

    Update SortBar.tsx

--- components/SortBar.tsx
+++ components/SortBar.tsx
@@ -1,12 +1,26 @@
-export function SortBar() {
+"use client";
+import React from "react";
+
+interface SortBarProps {
+  options?: string[];
+  onSortChange?: (option: string) => void;
+}
+
+const SortBar: React.FC<SortBarProps> = ({ options = ["Price: Low to High", "Price: High to Low"], onSortChange }) => {
   return (
     <div className="flex justify-end mb-4">
-      <select className="border border-gray-300 rounded-md p-2">
-        <option value="relevance">Sort by Relevance</option>
-        <option value="priceLow">Price: Low to High</option>
-        <option value="priceHigh">Price: High to Low</option>
-        <option value="rating">Rating</option>
+      <select
+        className="border px-3 py-2 rounded-lg"
+        onChange={(e) => onSortChange && onSortChange(e.target.value)}
+      >
+        {options.map((opt, idx) => (
+          <option key={idx} value={opt}>
+            {opt}
+          </option>
+        ))}
       </select>
     </div>
   );
-}
+};
+
+export default SortBar;
