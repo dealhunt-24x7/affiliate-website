0d97655dfe981e1ccb776a06d17866f5ba1c6d06
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-03 10:57:22 +0530

    Update seo.ts

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,1 +1,16 @@
+export const generateMetaTags = (title: string, description: string) => {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "website",
+      url: typeof window !== "undefined" ? window.location.href : "",
+    },
+  };
+};
 
+export const generateJsonLd = (data: object) => {
+  return JSON.stringify(data);
+};
