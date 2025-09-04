e16d44cbbb332936e4b37e52afee440af12ea679
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-04 11:26:12 +0530

    Update CategoryPills.tsx

--- components/CategoryPills.tsx
+++ components/CategoryPills.tsx
@@ -1,1 +1,37 @@
+"use client";
 
+import React from "react";
+
+interface Props {
+  categories?: string[];
+}
+
+const CategoryPills: React.FC<Props> = ({ categories = [] }) => {
+  const defaultCategories = [
+    "Electronics",
+    "Fashion",
+    "Home",
+    "Beauty",
+    "Toys",
+    "Sports",
+    "Books",
+    "Grocery",
+  ];
+
+  const allCategories = categories.length > 0 ? categories : defaultCategories;
+
+  return (
+    <div className="flex gap-3 overflow-x-auto no-scrollbar pb-3">
+      {allCategories.map((cat, idx) => (
+        <span
+          key={idx}
+          className="px-4 py-2 text-sm bg-gray-200 rounded-full cursor-pointer hover:bg-gray-300 whitespace-nowrap"
+        >
+          {cat}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default CategoryPills;
