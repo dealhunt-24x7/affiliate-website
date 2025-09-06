04bade59fd74d8bbcaf502bb3bf972bb8e6cdb49
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-06 17:31:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "../styles/globals.css";
-import Header from "@/components/Navbar";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata = {
