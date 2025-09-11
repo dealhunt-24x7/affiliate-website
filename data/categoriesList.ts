cfe0dca602ea606904affdc0a3b5a801d6dc3617
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-11 22:37:19 +0530

    Create categoriesList.ts

---
+++ data/categoriesList.ts
@@ -0,0 +1,20 @@
+// data/categoriesList.ts
+export const categories = [
+  { name: "Mobile", slug: "mobile", image: "/images/categories/mobile.png" },
+  { name: "Laptop", slug: "laptop", image: "/images/categories/laptop.png" },
+  { name: "Headphones", slug: "headphones", image: "/images/categories/headphones.png" },
+  { name: "Watches", slug: "watches", image: "/images/categories/watch.png" },
+  { name: "Electronics", slug: "electronics", image: "/images/categories/electronics.png" },
+  { name: "Men", slug: "men", image: "/images/categories/men.png" },
+  { name: "Women", slug: "women", image: "/images/categories/women.png" },
+  { name: "Kids", slug: "kids", image: "/images/categories/kids.png" },
+  { name: "Fashion", slug: "fashion", image: "/images/categories/fashion.png" },
+  { name: "Footwear", slug: "footwear", image: "/images/categories/footwear.png" },
+  { name: "Home appliances", slug: "home-appliances", image: "/images/categories/home-appliances.png" },
+  { name: "Sports", slug: "sports", image: "/images/categories/sports.png" },
+  { name: "Jwellery", slug: "jwellery", image: "/images/categories/jwellery.png" },
+  { name: "Kitchen", slug: "kitchen", image: "/images/categories/kitchen.png" },
+  { name: "Home decor", slug: "home-decor", image: "/images/categories/home-decor.png" },
+  { name: "Study", slug: "study", image: "/images/categories/study.png" },
+  { name: "Others", slug: "others", image: "/images/categories/others.png" },
+];
