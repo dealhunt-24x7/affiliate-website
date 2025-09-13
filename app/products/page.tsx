b1645f24cd3295850d572261a145577baf16b937
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-13 11:59:18 +0530

    Update page.tsx

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,34 +1,20 @@
 "use client";
 export const dynamic = "force-dynamic";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CategoryRow from "@/components/CategoryRow";
+import { categories } from "@/data/categoriesList";
 
 type Category = {
   name: string;
   slug: string;
-  subcategories: string[];
+  image: string;
 };
 
 export default function ProductsPage() {
-  const [categories, setCategories] = useState<Category[]>([]);
-
-  useEffect(() => {
-    async function fetchCategories() {
-      try {
-        const res = await fetch("/api/categories");
-        const data = await res.json();
-        setCategories(data.categories); // ✅ JSON ke andar "categories" key h
-      } catch (error) {
-        console.error("Error loading categories:", error);
-      }
-    }
-    fetchCategories();
-  }, []);
-
   return (
     <div className="p-4 space-y-10">
-      {categories.map((cat, idx) => (
+      {(categories as Category[]).map((cat, idx) => (
         <CategoryRow key={idx} category={cat} />
       ))}
     </div>
