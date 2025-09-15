9802ceb318989a75061f9b6d582d1a868fd78215
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-16 01:44:46 +0530

    Update categories.ts

--- data/categories.ts
+++ data/categories.ts
@@ -1,1 +1,42 @@
-
+export const categories = [
+  {
+    name: "Men's Watches",
+    slug: "mens-watches",
+    image: "/images/categories/mens-watches.jpg",
+  },
+  {
+    name: "Women's Watches",
+    slug: "womens-watches",
+    image: "/images/categories/womens-watches.jpg",
+  },
+  {
+    name: "Jewelry",
+    slug: "jewelry",
+    image: "/images/categories/jewelry.jpg",
+  },
+  {
+    name: "Shoes",
+    slug: "shoes",
+    image: "/images/categories/shoes.jpg",
+  },
+  {
+    name: "Handbags",
+    slug: "handbags",
+    image: "/images/categories/handbags.jpg",
+  },
+  {
+    name: "Sunglasses",
+    slug: "sunglasses",
+    image: "/images/categories/sunglasses.jpg",
+  },
+  {
+    name: "Fragrances",
+    slug: "fragrances",
+    image: "/images/categories/fragrances.jpg",
+  },
+  {
+    name: "Apparel",
+    slug: "apparel",
+    image: "/images/categories/apparel.jpg",
+  },
+];
