790ec862843fac0ca54583e457ebec693a865cb9
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-16 01:06:32 +0530

    Delete lib directory

--- lib/analytics.ts
+++
@@ -1,4 +0,0 @@
-export const trackAffiliateClick = (link: string) => {
-  console.log(`Affiliate link clicked: ${link}`);
-  // TODO: integrate with analytics provider (Google Analytics, Plausible, etc.)
-};
--- lib/constants.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_NAME = "DealHunt";
-export const BASE_URL = "https://example.com";
