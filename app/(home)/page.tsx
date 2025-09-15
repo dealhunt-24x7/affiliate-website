72756abc62c83c124053f030dfb3f4dab24fae3d
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-16 01:40:55 +0530

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,1 +1,19 @@
+"use client";
 
+import HeroBanner from "@/components/HeroBanner";
+import CategoryGrid from "@/components/CategoryGrid";
+import FeaturedDeals from "@/components/FeaturedDeals";
+
+export default function HomePage() {
+  return (
+    <main className="space-y-12">
+      <HeroBanner />
+      <section className="px-4">
+        <CategoryGrid />
+      </section>
+      <section className="px-4">
+        <FeaturedDeals />
+      </section>
+    </main>
+  );
+}
