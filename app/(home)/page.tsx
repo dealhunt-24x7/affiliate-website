6a234c31fbce0c3757c254d7f5b74e76f7ab951a
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-21 01:14:38 +0530

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,14 +6,10 @@ import FeaturedDeals from "@/components/FeaturedDeals";
 
 export default function HomePage() {
   return (
-    <main className="space-y-12">
+    <div className="space-y-12">
       <HeroBanner />
-      <section className="px-4">
-        <CategoryGrid />
-      </section>
-      <section className="px-4">
-        <FeaturedDeals />
-      </section>
-    </main>
+      <CategoryGrid />
+      <FeaturedDeals />
+    </div>
   );
 }
