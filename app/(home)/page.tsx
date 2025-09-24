8aec8c3f2acc0fee152dd54ca6f18312bf2fd322
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-09-24 23:39:52 +0530

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -10,12 +10,23 @@ import FAQSection from "@/components/FAQSection";
 export default function HomePage() {
   return (
     <div className="space-y-12">
+      {/* Hero Banner */}
       <HeroBanner />
+
+      {/* Deal of the Day - Auto Scroll */}
       <DealOfTheDay />
+
+      {/* Categories */}
       <CategoryGrid />
+
+      {/* Featured Deals */}
       <FeaturedDeals />
+
+      {/* Blogs Section */}
       <BlogSection />
+
+      {/* FAQ Section */}
       <FAQSection />
-    </>
+    </div>
   );
 }
