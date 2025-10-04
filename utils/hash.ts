fe44b861412f400598d4723d6908b0edd002ebb8
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-10-05 00:21:15 +0530

    Update hash.ts

--- utils/hash.ts
+++ utils/hash.ts
@@ -2,9 +2,9 @@ import bcrypt from "bcryptjs";
 
 export async function hashPassword(password: string) {
   const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(password, salt);
+  return await bcrypt.hash(password, salt);
 }
 
-export async function verifyPassword(password: string, hashed: string) {
-  return bcrypt.compare(password, hashed);
+export async function verifyPassword(password: string, hashedPassword: string) {
+  return await bcrypt.compare(password, hashedPassword);
 }
