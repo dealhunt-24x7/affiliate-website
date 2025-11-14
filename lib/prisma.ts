36b688b90ddd1ed4bebec68aeef3ebf21f6843c1
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-11-14 09:47:27 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // eslint-disable-next-line no-var
+  // Allow global prisma in dev
   var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
