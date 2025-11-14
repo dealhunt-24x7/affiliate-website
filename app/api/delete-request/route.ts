2826dfb3193bc444657c94d694dd39f4bf8b80e7
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-11-14 11:10:55 +0530

    Update route.ts

--- app/api/delete-request/route.ts
+++ app/api/delete-request/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/option";
+import { authOptions } from "@/app/api/auth/options";
 import prisma from "@/lib/prisma";
 
 export async function POST() {
