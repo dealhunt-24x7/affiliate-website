88575ee9db0798cfe25424de46c1adb6dd82bf41
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-11-16 02:18:51 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
 import { authConfig } from "../options";
 
-export const GET = async (...args) => {
+export const GET = async (...args: any[]) => {
   const { NextAuth } = await import("next-auth");
   return NextAuth(authConfig).GET(...args);
 };
 
-export const POST = async (...args) => {
+export const POST = async (...args: any[]) => {
   const { NextAuth } = await import("next-auth");
   return NextAuth(authConfig).POST(...args);
 };
