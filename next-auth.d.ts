f4d7e2d7ff3ff226c9dfd20a15c4853a4d203dcb
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-11-17 04:24:16 +0530

    Update next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -11,8 +11,8 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      name?: string | null;
-      email?: string | null;
+      name: string | null;
+      email: string | null;
       image?: string | null;
     };
   }
