8147037816338b05cdd49e1cb5de04e11b17f236
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-11-18 02:22:17 +0530

    Delete app/api directory

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,5 +0,0 @@
-import { auth } from "@auth/core";
-import { authOptions } from "@/lib/authOptions";
-
-export const GET = (req: Request) => auth({ ...authOptions, req });
-export const POST = (req: Request) => auth({ ...authOptions, req });
