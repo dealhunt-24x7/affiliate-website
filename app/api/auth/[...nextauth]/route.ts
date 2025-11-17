b575a00fae73fb687649a885f1eb3ec336138c30
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-11-17 19:05:38 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,5 @@
-export { GET, POST } from "@/lib/auth";
+import { auth } from "@auth/core";
+import { authOptions } from "@/lib/authOptions";
+
+export const GET = (req: Request) => auth({ ...authOptions, req });
+export const POST = (req: Request) => auth({ ...authOptions, req });
