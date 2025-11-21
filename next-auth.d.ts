66dc8fa69a1fac39adf1c27ef41e7bcb3f2a9e67
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-11-21 22:05:59 +0530

    Update next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
   interface User {
@@ -8,10 +8,7 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      name?: string | null;
-      email?: string | null;
-      image?: string | null;
-    };
+    } & DefaultSession["user"];
   }
 }
 
@@ -19,4 +16,4 @@ declare module "next-auth/jwt" {
   interface JWT {
     id: string;
   }
-  }
+}
