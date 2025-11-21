b421f3deb09997a99c5713793d73dca36f1fe4af
dealhunt-24x7 <dealhunt24x7@gmail.com>
2025-11-21 21:11:16 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
-import { auth } from "next-auth";
+import NextAuth from "next-auth";
 import { authOptions } from "../options";
 
-export const GET = auth(authOptions);
-export const POST = auth(authOptions);
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
